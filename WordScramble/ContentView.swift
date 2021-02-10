f4e6ecc29bf78a653465c35c8c2cb0cba3797e7f
anubhavsingh16 <anubhavssingh177@gmail.com>
2021-02-10 15:07:59 +0530

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Pully on 10/02/21.
+//  Copyright © 2021 catalyst. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
